68a650aa52cace56d6d385479fb63275daab16ec
Varun Kale <varun08kale@gmail.com>
2023-06-30 21:07:59 +0530

    Included a IMG folder and CSS file
    Img folder and CSS file for the UI of the project.

--- script.js
+++ script.js
@@ -1,110 +1,82 @@
 const options = ["rock", "paper", "scissor"];
+let playerCount = 0;
+let cpuCount = 0;
 
-function getComputerChoice()
-{
-   const choice = options[Math.floor(Math.random()*options.length)];
-   
-   // console.log(choice);
-   return choice;
+// Get the choice from the CPU
+function getComputerChoice() {
+  const choice = options[Math.floor(Math.random() * options.length)];
+  return choice;
 }
 
-function checkWinner(playerSelection, computerSelection)
-{
-   if(playerSelection == computerSelection)
-   {
-      return "Tie";
-   }
-   else if(
-      (playerSelection == "rock" && computerSelection =="scissor") 
-      || 
-      (playerSelection== "scissor" && computerSelection =="paper") 
-      || (playerSelection=="paper" && computerSelection == "rock"))
-      {
-         return "Win";
+// Get the player's choice by getting the ID of the clicked button
+function handleButtonClick(event) {
+
+    // to freeze the scores, crucial conditon in freezing the scores, so when one of them reaches score of five , it will not increase the count as well display it if we try to press the buttons
+    // helps in disabling the functionality of the buttons
+    if (playerCount >= 5 || cpuCount >= 5) {
+        return; // Exit the function if the game is over
       }
-   else
-   {
-      return "Loss"
-   }
+
+    const playerSelection = event.target.id;
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
 }
 
-function playRound(playerSelection, computerSelection)
-{
-   const getResult = checkWinner(playerSelection, computerSelection);
-   if(getResult == "Tie")
-   {
-      console.log("Its a tie");
-   }
-   else if(getResult == "Win")
-   {
-      console.log(`You win! ${playerSelection} beats ${computerSelection} `);
-   }
-   else
-   {
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}`)
-   }
+// Attach event listeners to the buttons
+document.getElementById("rock").addEventListener("click", handleButtonClick);
+document.getElementById("paper").addEventListener("click", handleButtonClick);
+document.getElementById("scissor").addEventListener("click", handleButtonClick);
+
+// Function to check the winner
+function getWinner(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissor") ||
+    (playerSelection === "scissor" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")
+  ) {
+    return "Win";
+  } else {
+    return "Loss";
+  }
 }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+// Function to play rounds and declare the overall winner
+function playRound(playerSelection, computerSelection) {
+  const result = getWinner(playerSelection, computerSelection);
 
-function getPlayerChoice()
-{
-   let validInput = false;
 
-   while(validInput == false)
-   {
-      const choice = prompt("Rock Paper Scissor");
+  if (result === "Win") {
+    playerCount++;
+    document.getElementById("player-score").innerText = playerCount;
+    document.getElementById("result-text").innerText = "You Win!";
+  } else if (result === "Loss") {
+    cpuCount++;
+    document.getElementById("cpu-score").innerText = cpuCount;
+    document.getElementById("result-text").innerText = "You Lose!";
+  } else {
+    document.getElementById("result-text").innerText = "It's a tie!";
+  }
 
-      if(choice == null)
-      {
-         continue;
-      }
-
-      const choiceInLower = choice.toLowerCase();
-
-      if(options.includes(choiceInLower))
-      {
-         validInput = true;
-         return choiceInLower;
-      }
-   }
+  if (playerCount === 5) {
+    document.getElementById("result-text").innerText = "You Won the Game! Play Again :)";
+  } else if (cpuCount === 5) {
+    document.getElementById("result-text").innerText = "You Lost the Game. Play Again :(";
+  }
 }
 
-function game()
-{
-   let scorePlayer = 0;
-   let scoreCpu = 0;
-   for(let i = 0; i< 5; i++)
-   {
-      
-      console.log(`Round ${i+1}`);
-      const playerSelection = getPlayerChoice();
-      const computerSelection = getComputerChoice();
-      playRound(playerSelection, computerSelection);
-      if(checkWinner(playerSelection, computerSelection) == "Win")
-      {
-         scorePlayer++;
-      }
-      else if(checkWinner(playerSelection, computerSelection) == "Lose")
-      {
-         scoreCpu++;
-      }
-   }
-   console.log("Game Over!");
-   if(scorePlayer == scoreCpu)
-   {
-      console.log("Its a TIE!");
-   }
-   else if(scorePlayer > scoreCpu)
-   {
-      console.log("You Win!");
-   }
-   else 
-   {
-      console.log("You Lose!");
-   }
-}
+// function for the play again button where we reset and display the player and cpu count to zero and change the result text 
 
-game();
+const playAgain = document.getElementById("play-again");
+
+playAgain.addEventListener("click", playAgainFunc);
+
+function playAgainFunc(){
+    // console.log("hello mate");
+    playerCount = 0;
+    cpuCount = 0;
+    document.getElementById("result-text").innerText = "Lets Begin!";
+    document.getElementById("player-score").innerText = playerCount;
+    document.getElementById("cpu-score").innerText = cpuCount;
+}
